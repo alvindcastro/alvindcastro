b3ed10ea2cb1a0378c31b5a1d2dc8b6b437d0199
De Castro Alvin Arceo  (NCS) <alvin.decastro@ncs.com.sg>
2018-11-19 15:31:37 +0800

    Refactor

--- travel-echo-mongo/model/user.go
+++ travel-echo-mongo/model/user.go
@@ -6,10 +6,9 @@ import (
 
 type (
 	User struct {
-		ID        bson.ObjectId `json:"id" bson:"_id,omitempty"`
-		Email     string        `json:"email" bson:"email"`
-		Password  string        `json:"password,omitempty" bson:"password"`
-		Token     string        `json:"token,omitempty" bson:"-"`
-		Followers []string      `json:"followers,omitempty" bson:"followers,omitempty"`
+		ID       bson.ObjectId `json:"id" bson:"_id,omitempty"`
+		Email    string        `json:"email" bson:"email"`
+		Password string        `json:"password,omitempty" bson:"password"`
+		Token    string        `json:"token,omitempty" bson:"-"`
 	}
 )
